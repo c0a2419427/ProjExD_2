e822eba436649c9f0063e9b2561628884a473274
c0a2419427 <c0a2419427@edu.teu.ac.jp>
2025-04-22 15:00:16 +0900

    練習問題4

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -55,7 +55,12 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return
+       
         screen.blit(bg_img, [0, 0]) 
+   
+        if kk_rct.colliderect(bb_rct): 
+            print("Game Over")
+            return
 
         key_lst = pg.key.get_pressed()
         sum_mv = [0, 0]
